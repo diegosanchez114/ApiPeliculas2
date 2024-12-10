0453d7e212bb37237de6b143f67c0de91ab95ec4
ani\dsanchez <dsanchez@ani.gov.co>
2024-12-10 17:05:47 -0500

    Primer Commit

---
+++ ApiPeliculas/ApiPeliculas/Data/AplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ApiPeliculas.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ApiPeliculas.Data
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options)    
+        {
+            
+        }
+
+        //Aqui pasamos los modelos
+        public DbSet<Categoria> Categorias { get; set; }    
+    }
+}
